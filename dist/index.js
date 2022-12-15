b705d72e3d24249bd6430871aeab09712a877b40
Jordan Wright <wright.jordan.n@outlook.com>
2022-12-15 16:27:30 -0600

    first commit

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+import { createHash } from "crypto";
+/** Sets cache-control and etag headers. Ends response with status code 304 if etags match. */
+export function etag(data, strategy, req, res) {
+    res.setHeader("Cache-Control", strategy);
+    const oldETag = req.headers["if-none-match"] ?? "";
+    const newETag = createHash("sha1").update(data).digest("hex");
+    res.setHeader("ETag", newETag);
+    if (oldETag === newETag) {
+        res.statusCode = 304;
+        res.end("");
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { createHash } from "crypto";
+import type { IncomingMessage, ServerResponse } from "http";
+
+/** Sets cache-control and etag headers. Ends response with status code 304 if etags match. */
+export function etag(
+  data: string,
+  strategy: string,
+  req: IncomingMessage,
+  res: ServerResponse
+) {
+  res.setHeader("Cache-Control", strategy);
+  const oldETag = req.headers["if-none-match"] ?? "";
+  const newETag = createHash("sha1").update(data).digest("hex");
+  res.setHeader("ETag", newETag);
+  if (oldETag === newETag) {
+    res.statusCode = 304;
+    res.end("");
+  }
+}
