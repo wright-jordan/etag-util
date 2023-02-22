1af86931ab191373ad258dfe883639de9a76b751
Jordan Wright <wright.jordan.n@outlook.com>
2023-02-22 01:31:54 -0600

    dont end reponse

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
 /// <reference types="node" />
 import type { IncomingMessage, ServerResponse } from "http";
-/** Sets cache-control and etag headers. Ends response with status code 304 if etags match. */
-export declare function etag(data: string, strategy: string, req: IncomingMessage, res: ServerResponse): void;
+/** Sets cache-control and etag headers. Returns true if tags match. */
+export declare function etag(data: string, strategy: string, req: IncomingMessage, res: ServerResponse): boolean;
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 import { createHash } from "crypto";
 import type { IncomingMessage, ServerResponse } from "http";
 
-/** Sets cache-control and etag headers. Ends response with status code 304 if etags match. */
+/** Sets cache-control and etag headers. Returns true if tags match. */
 export function etag(
   data: string,
   strategy: string,
   req: IncomingMessage,
   res: ServerResponse
-) {
+): boolean {
   res.setHeader("Cache-Control", strategy);
   const oldETag = req.headers["if-none-match"] ?? "";
   const newETag = createHash("sha1").update(data, "utf-8").digest("hex");
   res.setHeader("ETag", newETag);
   if (oldETag === newETag) {
-    res.statusCode = 304;
-    res.end("");
+    return true;
   }
+  return false;
 }
